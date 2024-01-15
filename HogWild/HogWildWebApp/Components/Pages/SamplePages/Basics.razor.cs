31ebd81db2dd39d0da3a6fd223f761eb06047a8c
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-01-15 14:43:42 -0700

    End of Day

--- HogWild/HogWildWebApp/Components/Pages/SamplePages/Basics.razor.cs
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/Basics.razor.cs
@@ -1,4 +1,6 @@
-﻿namespace HogWildWebApp.Components.Pages.SamplePages
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HogWildWebApp.Components.Pages.SamplePages
 {
     public partial class Basics
     {
@@ -9,7 +11,41 @@
         private int oddEvenValue;
         #endregion
 
+        #region Text Boxes
+        //  email address
+        private string emailText;
+        //  password
+        private string passwordText;
+        //  date 
+        private DateTime? dateText = DateTime.Today;
+        #endregion
+
+        #region Radio Buttons, Checkboxes & Text Area
+        //  selected meal
+        private string meal = "breakfast";
+        private string[] meals { get; set; } = new string[] { "breakfast", "lunch", "dinner", "snacks" };
+        //  used to hold the value of the acceptance
+        private bool acceptanceBox;
+        // used to hold the value for the message body
+        private string messageBody;
+        #endregion
+
+        //  used to display any feedback to the end user.
+        private string feedback;
+
         #region Methods
+        // This method is automatically called when the component is initialized.
+        protected override async Task OnInitializedAsync()
+        {
+            // Call the base class OnInitializedAsync method (if any).
+            await base.OnInitializedAsync();
+
+            // Call the 'RandomValue' method to perform custom initialization logic.
+            RandomValue();
+
+            //// Call the 'PopulatedList' method to populate predefined data for the list.
+            //PopulatedList();
+        }
         // Generates a random number between 0 and 25 using the Random class
         // Checks if the generated number is even
         // Sets the 'myName' variable to a message if even, or to null if odd
@@ -36,7 +72,32 @@
             // Trigger an asynchronous update of the component's state to reflect the changes made.
             InvokeAsync(StateHasChanged);
         }
-        #endregion
 
+        //  This method is called when a user submits text input.
+        private void TextSubmit()
+        {
+            // Combine the values of emailText, passwordText, and dateText into a feedback message.
+            feedback = $"Email {emailText}; Password {passwordText}; Date {dateText}";
+
+            // Trigger a re-render of the component to reflect the updated feedback.
+            InvokeAsync(StateHasChanged);
+        }
+
+        // Handle the selection of the loop meal
+        private void HandleMealSelection(ChangeEventArgs e)
+        {
+            meal = e.Value.ToString();
+        }
+
+        //  This method is called when a user submits radio, check box and area text.
+        private void RadioCheckAreaSubmit()
+        {
+            // Combine various values and store them in the 'feedback' variable as a formatted string.
+            feedback = $"Meal {meal}; Acceptance {acceptanceBox}; Message {messageBody}";
+
+            // Trigger a UI update to reflect the changes made to the 'feedback' variable.
+            InvokeAsync(StateHasChanged);
+        }
+        #endregion
     }
 }
