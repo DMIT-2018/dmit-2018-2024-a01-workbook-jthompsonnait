9427372f07c52400f8b5fa85bc5150727018ffbe
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-03-21 10:59:02 -0600

    End if Day March 19

--- HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
+++ HogWild/HogWildWebApp/Components/Pages/SamplePages/CustomerEdit.razor.cs
@@ -2,6 +2,8 @@
 using HogWIldSystem.ViewModels;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Mono.TextTemplating;
+using System.Drawing;
 
 namespace HogWildWebApp.Components.Pages.SamplePages
 {
@@ -16,9 +18,22 @@ namespace HogWildWebApp.Components.Pages.SamplePages
         private List<LookupView> countries = new();
         //  The status lookup
         private List<LookupView> statusLookup = new();
+
+
+        #endregion
+
+        #region Validation
+        private string closeButtonText = "Close";
         // the edit context
         private EditContext editContext;
-        private string closeButtonText = "Close";
+
+        //  disable save button
+        private bool disableSaveButton => !editContext.IsModified() || !editContext.Validate();
+
+        //  used to store the validation message
+        private ValidationMessageStore messageStore;
+
+
         #endregion
 
         #region Feedback & Error Messages
@@ -46,6 +61,14 @@ namespace HogWildWebApp.Components.Pages.SamplePages
         //  The category lookup service
         [Inject] protected CategoryLookupService CategoryLookupService { get; set; }
 
+        //   Injects the NavigationManager dependency
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        // the dialog service
+        //[Inject]
+        //protected IDialogService DialogService { get; set; }
+
         //  Customer ID used to create or edit a customer
         [Parameter] public int CustomerID { get; set; } = 0;
         #endregion
@@ -55,6 +78,19 @@ namespace HogWildWebApp.Components.Pages.SamplePages
             await base.OnInitializedAsync();
             try
             {
+                //  edit context needs to be setup after data has been initialized
+                //  setup of the edit context to make use of the payment type property
+                editContext = new EditContext(customer);
+
+                //  set the validation to use the HandleValidationRequest event
+                editContext.OnValidationRequested += HandleValidationRequested;
+
+                //  setup the message store to track any validation messages
+                messageStore = new ValidationMessageStore(editContext);
+
+                //  this event will fire each time the data in a property has change.
+                editContext.OnFieldChanged += EditContext_OnFieldChanged;
+
                 //  reset the error detail list
                 errorDetails.Clear();
 
@@ -64,9 +100,7 @@ namespace HogWildWebApp.Components.Pages.SamplePages
                 //  reset feedback message to an empty string
                 feedbackMessage = String.Empty;
 
-                //  edit context needs to be setup after data has been initialized
-                //  setup of the edit context to make use of the payment type property
-                editContext = new EditContext(customer);
+
                 //  check to see if we are navigating using a valid customer CustomerID.
                 //      or are we going to create a new customer.
                 if (CustomerID > 0)
@@ -79,6 +113,7 @@ namespace HogWildWebApp.Components.Pages.SamplePages
                 countries = CategoryLookupService.GetLookups("Country");
                 statusLookup = CategoryLookupService.GetLookups("Customer Status");
 
+                await InvokeAsync(StateHasChanged);
             }
             catch (ArgumentNullException ex)
             {
@@ -105,6 +140,45 @@ namespace HogWildWebApp.Components.Pages.SamplePages
             }
         }
 
+        // Handles the validation requested.        
+        private void HandleValidationRequested(object sender, ValidationRequestedEventArgs e)
+        {
+            //  clear the message store if there is any existing validation errors.
+            messageStore?.Clear();
+
+            //  custom validation logic
+            //  first name is required
+            if (string.IsNullOrWhiteSpace(customer.FirstName))
+            {
+                messageStore?.Add(() => customer.FirstName, "First Name is required!");
+            }
+            //  last name is required
+            if (string.IsNullOrWhiteSpace(customer.LastName))
+            {
+                messageStore?.Add(() => customer.LastName, "Last Name is required!");
+            }
+            //  phone is required
+            if (string.IsNullOrWhiteSpace(customer.Phone))
+            {
+                messageStore?.Add(() => customer.Phone, "Phone is required!");
+            }
+            //  email is required
+            if (string.IsNullOrWhiteSpace(customer.Email))
+            {
+                messageStore?.Add(() => customer.Email, "Email is required!");
+            }
+        }
+
+        // Handles the OnFieldChanged event of the EditContext control.
+        private void EditContext_OnFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            //  the "editContext.Validate()" should not be needed
+            //    but if the "HandleValidationRequested" does not fire on it own
+            //    you will need to add it.  
+            editContext.Validate();
+            closeButtonText = editContext.IsModified() ? "Cancel" : "Close";
+        }
+
         // save the customer
         private void Save()
         {
@@ -118,7 +192,8 @@ namespace HogWildWebApp.Components.Pages.SamplePages
             feedbackMessage = String.Empty;
             try
             {
-
+                customer = CustomerService.Save(customer);
+                feedbackMessage = "Data was successfully saved!";
             }
             catch (ArgumentNullException ex)
             {
@@ -146,7 +221,7 @@ namespace HogWildWebApp.Components.Pages.SamplePages
 
         private async void Cancel()
         {
-
+            NavigationManager.NavigateTo("/SamplePages/CustomerList");
         }
     }
 }
